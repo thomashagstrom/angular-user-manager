ce6fe74163d0afa6de13c3221a3e10b33d308a9f
Thomas Hagström <thomashagstrom@outlook.com>
2026-06-01 08:20:19 +0200

    feat: update users page to include user links for navigation

--- src/features/users/pages/users-page.ts
+++ src/features/users/pages/users-page.ts
@@ -1,11 +1,13 @@
 import { Component, inject } from '@angular/core';
 import { Users } from '../services/users';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-users-page',
   standalone: true,
   templateUrl: './users-page.html',
   styleUrl: './users-page.css',
+  imports: [RouterLink],
 })
 export class UsersPage {
   private usersService = inject(Users);
