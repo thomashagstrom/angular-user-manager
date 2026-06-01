30113bafaef1c32ac83b8e99e1a2ea628a2e3b3c
Thomas Hagström <thomashagstrom@outlook.com>
2026-06-01 11:14:55 +0200

    feat: refactor user display to use material cards and update styles

--- src/features/users/pages/users-page.ts
+++ src/features/users/pages/users-page.ts
@@ -1,13 +1,16 @@
 import { Component, inject } from '@angular/core';
 import { Users } from '../services/users';
 import { RouterLink } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
 
 @Component({
   selector: 'app-users-page',
   standalone: true,
   templateUrl: './users-page.html',
   styleUrl: './users-page.css',
-  imports: [RouterLink],
+  imports: [RouterLink, MatButtonModule, MatCardModule, MatDividerModule],
 })
 export class UsersPage {
   private usersService = inject(Users);
